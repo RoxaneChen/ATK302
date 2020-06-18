3561d6f1215ff8d12d183abf2472705eb492c542
RoxaneChen <3060956894@qq.com>
2020-06-18 14:46:30 +0800

    1

--- p5/Unit1/scene/sketch.js
+++ p5/Unit1/scene/sketch.js
@@ -1,21 +1,41 @@
 function setup() {
-  createCanvas(500, 500);
-  
+    // Sets the screen to be 720 pixels wide and 400 pixels high
+  createCanvas(720, 400);
+  background("yellow");
+  noStroke();
+
+  fill('green');
+  triangle(65, 120, 16, 220, 120, 220);
+
+  fill('green');
+  rect(45, 210, 50, 60);
+
+  fill('blue');
+  rect(378, 120, 42, 80);
+
+  fill(255);
+  ellipse(400, 200, 80, 80);
+
+  fill('blue');
+  rect(360, 150, 82, 20);
+
+  fill(255);
+  ellipse(400, 280, 120, 120);
+
+  fill('black');
+  ellipse(380, 200, 15, 15);
+
+  fill('black');
+  ellipse(420, 200, 15, 15);
+
+  fill('black');
+  ellipse(400, 260, 12, 12);
+
+  fill('black');
+  ellipse(400, 285, 12, 12);
+
+  fill('black');
+  ellipse(400, 310, 12, 12);
+
+
 }
-
-function draw() {
-  background(100) ; 
-  // put your shapes here!
-  
-  
-  text(mouseX + " , " + mouseY, 10, 20) ;  // this helps know where the mouse is
-} 
-
-  
-
-function mouseReleased() { 
-  // if you click the mouse, you'll see the location in the debugging console
-  print(mouseX + " , " + mouseY) ; 
-} 
-
- 
